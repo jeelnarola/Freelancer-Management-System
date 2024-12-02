329872c43d324b383b282b592b8d4967661e126d
jeelnarola <narolajeel29@gmail.com>
2024-12-02 10:43:53 +0530

    Payment Method

--- controllers/freelancer.controller.js
+++ controllers/freelancer.controller.js
@@ -1,6 +1,17 @@
 const csv = require('fast-csv');
+const { Parser } = require('json2csv');
+const fs = require('fs');
+const Rezorpay=require("razorpay")  
 const validateExpenseData = require('../utils/validatore');
 const Freelancer = require('../models/freelancer.Schema');
+const PaymentModel = require('../models/pyment.Schema');
+require('dotenv').config()
+
+
+const razorpay=new Rezorpay({
+    key_id:process.env.RZP_KEY,
+    key_secret:process.env.RZP_SECRET
+}) 
 
 const AllProjectShow = async (req, res) => {
     try {
@@ -81,4 +92,54 @@ const ProjectDelete = async(req,res)=>{
     }
 }
 
-module.exports = { ProjectCreaet, AllProjectShow, ProjectUpdate, ProjectDelete }
+const ProjectExport = async(req,res)=>{
+    try {
+        const fields = ['title', 'description', 'userId','dueDate','status'];
+        const json2csvParser = new Parser({ fields });
+        const csv = json2csvParser.parse(data);
+        res.setHeader('Content-Type', 'text/csv');
+        res.setHeader('Content-Disposition', 'attachment; filename="data.csv"');
+        res.status(200).send(csv);
+    } catch (error) {
+        console.log("Error In ProjectExport Controller :- ",error.message)
+        res.status(400).json({message:'Internal Error :- ',error:error})
+    }
+}
+
+const Payment =async(req,res)=>{
+    let {userid,projectid,amount} = req.body;
+    console.log(process.env.RZP_KEY)
+    try {
+        let option={
+            amount:amount*100,
+        }
+        razorpay.orders.create(option,async(err,order)=>{
+            console.log("Errrr",order)
+
+            if(err){
+                res.send(err)
+            }
+            if(order){
+                let data={
+                  success:true,
+                  msg:"order create",
+                  order_id:order.id,
+                  key_id:process.env.RZP_KEY,
+                  name:"jeel",
+                  amount:option.amount
+                }
+                await PaymentModel.create({
+                    userid,amount,status:"completed"
+                })
+               return res.status(200).json({success:true,message:'Payment SuccessFully Done...',PatmentData : data})
+              }
+        })
+        // res.status(200).json({success:true,message:'Payment SuccessFully Done...'})
+
+    } catch (error) {
+        console.log("Error In Payment Controller :- ",error.message)
+        res.status(400).json({message:'Internal Error :- ',error:error})
+    }
+}
+
+module.exports = { ProjectCreaet, AllProjectShow, ProjectUpdate, ProjectDelete, ProjectExport, Payment }
--- routers/freelancer.router.js
+++ routers/freelancer.router.js
@@ -1,6 +1,6 @@
 const {Router} = require('express')
 const multer =require('multer')
-const {ProjectCreaet, AllProjectShow, ProjectUpdate, ProjectDelete} = require('../controllers/freelancer.controller')
+const {ProjectCreaet, AllProjectShow, ProjectUpdate, ProjectDelete, ProjectExport, Payment} = require('../controllers/freelancer.controller')
 const FRouter = Router()
 
 const upload = multer({dest:'uplods/'})
@@ -9,5 +9,8 @@ FRouter.get('/prject/show',AllProjectShow)
 FRouter.post('/project/create',upload.single('file'),ProjectCreaet)
 FRouter.patch('/project/update/:id',ProjectUpdate)
 FRouter.delete('/project/delete',ProjectDelete)
+FRouter.post('/project/export',ProjectExport)
+
+FRouter.post('/payment',Payment)
 
 module.exports = FRouter
